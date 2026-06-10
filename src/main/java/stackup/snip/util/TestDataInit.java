d0180edd6846b809ca4a0b63e3164920e7edc114
이민수 <minsoo.james1101@gmail.com>
2026-06-10 09:42:38 +0900

    category 페이징 기능 완성

--- src/main/java/stackup/snip/util/TestDataInit.java
+++ src/main/java/stackup/snip/util/TestDataInit.java
@@ -5,8 +5,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import stackup.snip.entity.Answer;
+import stackup.snip.entity.Category;
 import stackup.snip.entity.Member;
 import stackup.snip.repository.jpa.AnswerJpaRepository;
+import stackup.snip.repository.jpa.CategoryJpaRepository;
 import stackup.snip.repository.jpa.MemberJpaRepository;
 import stackup.snip.repository.jpa.SubjectiveJpaRepository;
 import stackup.snip.service.SubjectiveService;
@@ -19,6 +21,7 @@ import java.time.LocalDateTime;
 public class TestDataInit implements CommandLineRunner {
 
     private final MemberJpaRepository memberJpaRepository;
+    private final CategoryJpaRepository categoryJpaRepository;
     private final SubjectiveService subjectiveService;
 
     @Override
@@ -54,6 +57,12 @@ public class TestDataInit implements CommandLineRunner {
                 LocalDateTime.now()
             );
             memberJpaRepository.save(member);
+        }
+        for (int i = 0; i < 100; i++) {
+            Category member = new Category(
+                    "test" + i
+            );
+            categoryJpaRepository.save(member);
         }
         subjectiveService.importFromNotion();
     }
